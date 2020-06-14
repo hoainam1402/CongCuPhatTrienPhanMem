13ce4f996134999c9a54b2479899c8ef6752b18c
zonbita <zonbita96@gmail.com>
2020-06-14 12:26:30 +0700

    update login.php

--- login.php
+++ login.php
@@ -5,10 +5,10 @@
  <meta charset="utf-8">
  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Jquery -->
+  
     <script src="lib/Jquery/Jquery.min.js"></script>
+    
 
-    <!-- Bootstrap -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
